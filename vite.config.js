a1817580d246f7379d57c7639aafa91dcecfa073
Charlon Palacay <charlon@gmail.com>
2021-06-15 09:12:51 -0700

    Update display text.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
-module.exports = {
-    build: {
-      assetsDir: 'zoom/assets'
-    }
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    assetsDir: 'zoom/assets'
   }
-  
+})
